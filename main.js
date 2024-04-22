e9a5ab4a964f563686388b6dc2f7f2b6db29fa4b
Muhammed Rajab <muhammedrajab101@gmail.com>
2024-04-23 00:29:38 +0530

    animation on the cards is tricky

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 import "./style.css";
+import gsap from "gsap";
+
+const workCards = [...document.querySelectorAll(".work-card")];
+
+workCards.forEach((card) => {
+  card.addEventListener("mouseenter", () => {
+    const details = card.querySelector(".work-card-details");
+
+    gsap.to(details, {
+      ease: "power4.inOut",
+      duration: 0.2,
+      opacity: 1,
+    });
+  });
+
+  card.addEventListener("mouseleave", () => {
+    const details = card.querySelector(".work-card-details");
+
+    gsap.to(details, {
+      ease: "power4.inOut",
+      duration: 0.2,
+      opacity: 0,
+    });
+  });
+});
