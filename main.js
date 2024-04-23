0e2c09fb160bd824ce7fb45907b614e1a0fc1260
Muhammed Rajab <muhammedrajab101@gmail.com>
2024-04-23 10:14:50 +0530

    will think about styling later, now let's add a category bar

--- main.js
+++ main.js
@@ -6,23 +6,23 @@ const workCards = [...document.querySelectorAll(".work-card")];
 workCards.forEach((card) => {
   const details = card.querySelector(".work-card-details");
 
-  // card.addEventListener("mouseenter", () => {
-  //   gsap.fromTo(
-  //     details,
-  //     {
-  //       opacity: 0,
-  //       duration: 0.75,
-  //       ease: "power4.inOut",
-  //     },
-  //     { bottom: 0, opacity: 1 }
-  //   );
-  // });
+  card.addEventListener("mouseenter", () => {
+    gsap.fromTo(
+      details,
+      {
+        opacity: 0,
+        duration: 0.75,
+        ease: "power4.inOut",
+      },
+      { bottom: 0, opacity: 1 }
+    );
+  });
 
-  // card.addEventListener("mouseleave", () => {
-  //   gsap.to(details, {
-  //     opacity: 0,
-  //     duration: 0.75,
-  //     ease: "power4.inOut",
-  //   });
-  // });
+  card.addEventListener("mouseleave", () => {
+    gsap.to(details, {
+      opacity: 0,
+      duration: 0.75,
+      ease: "power4.inOut",
+    });
+  });
 });
