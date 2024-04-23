074738e48b6f21bfec6458e116d6715e40904de1
Muhammed Rajab <muhammedrajab101@gmail.com>
2024-04-23 10:12:57 +0530

    why is this card designing so frustrating

--- main.js
+++ main.js
@@ -4,23 +4,25 @@ import gsap from "gsap";
 const workCards = [...document.querySelectorAll(".work-card")];
 
 workCards.forEach((card) => {
-  card.addEventListener("mouseenter", () => {
-    const details = card.querySelector(".work-card-details");
+  const details = card.querySelector(".work-card-details");
 
-    gsap.to(details, {
-      ease: "power4.inOut",
-      duration: 0.2,
-      opacity: 1,
-    });
-  });
+  // card.addEventListener("mouseenter", () => {
+  //   gsap.fromTo(
+  //     details,
+  //     {
+  //       opacity: 0,
+  //       duration: 0.75,
+  //       ease: "power4.inOut",
+  //     },
+  //     { bottom: 0, opacity: 1 }
+  //   );
+  // });
 
-  card.addEventListener("mouseleave", () => {
-    const details = card.querySelector(".work-card-details");
-
-    gsap.to(details, {
-      ease: "power4.inOut",
-      duration: 0.2,
-      opacity: 0,
-    });
-  });
+  // card.addEventListener("mouseleave", () => {
+  //   gsap.to(details, {
+  //     opacity: 0,
+  //     duration: 0.75,
+  //     ease: "power4.inOut",
+  //   });
+  // });
 });
